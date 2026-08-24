8c1408c8b580e2e8b57988c8bd834c75e57cfafe
maupatrades-cmd <business.lekgoro@gmail.com>
2026-08-24 18:04:52 +0200

    feat(tasks): add governed role paperwork workspace

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,8 @@ import LeadReferrerPipelinePage from "@/pages/lead-referrer/LeadReferrerPipeline
 import ClientInvitationsPage from "@/pages/ClientInvitationsPage";
 import PortalBookingPage from "@/pages/portal/PortalBookingPage";
 import LeadReferrerBookingPage from "@/pages/lead-referrer/LeadReferrerBookingPage";
+import LeadReferrerTasksPage from "@/pages/lead-referrer/LeadReferrerTasksPage";
+import RoleDocumentTasksPage from "@/pages/portal/RoleDocumentTasksPage";
 import DashboardPage from "@/pages/DashboardPage";
 import PipelinePage from "@/pages/PipelinePage";
 import DealDetailPage from "@/pages/DealDetailPage";
@@ -182,6 +184,7 @@ function AppRoutes() {
         <Route path="submit-lead" element={<PartnerSubmitLeadPage />} />
         <Route path="leads" element={<PartnerMyLeadsPage />} />
         <Route path="deals" element={<PartnerDealsPage />} />
+        <Route path="tasks" element={<RoleDocumentTasksPage portal="partner" />} />
         <Route path="network" element={<PartnerNetworkPage surface="partner" />} />
         <Route path="client-invitations" element={<ClientInvitationsPage />} />
         <Route path="calendar" element={<PortalBookingPage portal="partner" />} />
@@ -212,6 +215,7 @@ function AppRoutes() {
         <Route path="submit-lead" element={<LeadReferrerSubmitLeadPage />} />
         <Route path="leads" element={<LeadReferrerLeadsPage />} />
         <Route path="pipeline" element={<LeadReferrerPipelinePage />} />
+        <Route path="tasks" element={<LeadReferrerTasksPage />} />
         <Route path="client-invitations" element={<ClientInvitationsPage />} />
         <Route path="calendar" element={<LeadReferrerBookingPage />} />
         <Route path="*" element={<Navigate to="/lead-referrer" replace />} />
